4fe41133d4708d8d13fac9e639ee9a7c1468db26
gorkemdurgun <gorkemdurgunn@gmail.com>
2024-01-22 23:15:44 +0300

    initial set, Add global SCSS styles and update dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,38 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import { useSpring, animated, useScroll } from "@react-spring/web";
+
+import styles from "@/styles/Home.module.scss";
+import { MutableRefObject, useRef } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [opProps, opApi] = useSpring(
+    () => ({
+      from: { opacity: 1 },
+      to: { opacity: 1 },
+    }),
+    []
+  );
+
+  const { scrollYProgress } = useScroll({
+    onChange: ({ value: { scrollYProgress } }) => {
+      if (scrollYProgress > 0.2) {
+        opApi.start({ opacity: 0 });
+      } else if (scrollYProgress > 0.4) {
+      } else if (scrollYProgress > 0.6) {
+      } else if (scrollYProgress > 0.8) {
+      } else {
+        // Default values
+        opApi.start({ opacity: 1 });
+      }
+    },
+    default: {
+      immediate: true,
+    },
+  });
   return (
     <>
       <Head>
@@ -14,99 +41,21 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <animated.div style={opProps} className={styles.launchSection}>
+            <div className={styles.launchSection__leftSide}>
+              <span className={styles.title}>
+                <h5> Hi, Im </h5> <h1> Jhonatan </h1>
+              </span>
+              <span className={styles.description}></span>
+            </div>
+            <div className={styles.launchSection__rightSide}>
+              <div className={styles.launchSection__rightSide__image}>
+                <Image src="/images/launch.svg" alt="Picture of the author" width={500} height={500} />
+              </div>
+            </div>
+          </animated.div>
         </div>
       </main>
     </>
